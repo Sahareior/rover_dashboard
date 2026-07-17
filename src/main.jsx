9d23873bc7e85d15c97893dc55ab13ab439c3867
Sahareior Sijan <sahareiors@gmail.com>
2026-07-17 08:30:50 +0600

    fixed: responsiveness

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx';
 import {
   createBrowserRouter,
   RouterProvider,
+  Navigate,
 } from 'react-router-dom';
 import RoverDashboard from './components/RoverDashboard/RoverDashboard.jsx';
 import Settings from './components/Settings/Settings.jsx';
@@ -13,6 +14,7 @@ const router = createBrowserRouter([
     path: '/',
     element: <App />,
     children: [
+      { index: true, element: <Navigate to="/rover-dashboard" replace /> },
       { path: '/rover-dashboard', element: <RoverDashboard /> },
       { path: '/settings', element: <Settings /> },
     ],
